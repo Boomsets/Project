b4294dad1e644b08d94969b71e2b61191ed3f76f
Boomsets <shepardkorol@gmail.com>
2024-10-11 16:59:14 +0300

    justlayout

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,12 @@
+import './Main.css';
+
 function Main () {
   return (    
   <body className="Main"> 
     <div className='ToDoList'>
       <div className="task">
         <p className="taskText">Task 1 Text</p>
-        <p className="taskText">Task 2 Text</p>
-        <p className="taskText">Task 3 Text</p>
+        <button className="taskStatusButton"></button>
       </div>
     </div>
   </body>
