cb3a2b2bca54957cd31f7cb8accf8bf5a8260df1
Boomsets <shepardkorol@gmail.com>
2024-10-13 21:43:04 +0300

    firstJScode

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,8 +3,12 @@ import './App.css';
 import Header from '../Header/Header.jsx';
 import Footer from '../Footer/Footer.jsx';
 import Main from '../Main/Main.jsx';
+import '../../fonts/fonts.css'
 
 function App() {
+  // user контекст
+  // user объект
+  // перенести массив Tasks и добавить его к user
   return (
     <div className="App">
       <Header/>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,16 +1,78 @@
+import { useState, useEffect } from 'react';
 import './Main.css';
 
-function Main () {
-  return (    
-  <body className="Main"> 
-    <div className='ToDoList'>
-      <div className="task">
-        <p className="taskText">Task 1 Text</p>
-        <button className="taskStatusButton"></button>
+function Main() {
+  const [task, setTask] = useState({ title: '' });
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = JSON.parse(localStorage.getItem('localStorageTasks'));
+    return savedTasks ? savedTasks : [];
+  });
+  
+  const [filter, setFilter] = useState('all'); // Фильтр задач
+
+  const handleInputChange = (evt) => {
+    setTask({ title: evt.target.value });
+  };
+
+  const handleAddTask = (evt) => {
+    evt.preventDefault();
+    if (task.title) {
+      const newTask = { title: task.title, id: Date.now(), completed: false };
+      setTasks([...tasks, newTask]);
+      setTask({ title: '' });
+    }
+  };
+
+  const toggleTaskCompletion = (id) => {
+    setTasks(tasks.map(task => 
+      task.id === id ? { ...task, completed: !task.completed } : task
+    ));
+  };
+
+  const handleFilterTasksChange = (newFilter) => {
+    setFilter(newFilter);
+  };
+
+  const filteredTasks = tasks.filter(task => {
+    if (filter === 'completed') return task.completed;
+    if (filter === 'incomplete') return !task.completed;
+    return true;
+  });
+
+  useEffect(() => {
+    localStorage.setItem('localStorageTasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  return (
+    <div className="Main">
+      <div className="toDoList">
+        <h2 className="listTitle">Список задач</h2>
+        <div className="filterButtons">
+          <button className='filterButton' onClick={() => handleFilterTasksChange('all')}>Все</button>
+          <button className='filterButton' onClick={() => handleFilterTasksChange('completed')}>Выполненные</button>
+          <button className='filterButton' onClick={() => handleFilterTasksChange('incomplete')}>Невыполненные</button>
+        </div>
+        {filteredTasks.map((item) => (
+          <div className="task" key={item.id}>
+            <p className={`taskText ${item.completed ? 'completed' : ''}`}>{item.title}</p>
+            <button className="taskStatusButton" onClick={() => toggleTaskCompletion(item.id)}>
+              {item.completed ? '✓' : '✗'}
+            </button>
+          </div>
+        ))}
       </div>
+      <form className="addNewTask" onSubmit={handleAddTask}>
+        <input
+          className="addNewTaskInput"
+          type="text"
+          onChange={handleInputChange}
+          value={task.title}
+          placeholder="Введите задачу"
+        />
+        <button type="submit" className="addNewTaskButton">Добавить</button>
+      </form>
     </div>
-  </body>
-  )
+  );
 }
 
 export default Main;
